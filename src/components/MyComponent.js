98e4c9fb241647bce7f826bc667852d6e4767fb4
ductt7575 <duc28072003@gmail.com>
2024-04-06 16:02:17 +0700

    Props in React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserInfor from './UserInfor';
+import DisplayInfo from './DisplayInfo';
 
 class MyComponent extends React.Component {
     //JSX: cho phép viết js bên trong code html
@@ -7,6 +8,11 @@ class MyComponent extends React.Component {
         return (
             <div>
                 <UserInfor></UserInfor>
+                <br />
+                <br />
+                <DisplayInfo name="Đức Trọng" age="18"></DisplayInfo>
+                <hr />
+                <DisplayInfo name="Người lạ ơi" age="38"></DisplayInfo>
             </div>
         );
     }
