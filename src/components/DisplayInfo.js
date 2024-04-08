23743c296ab4e20d83a2c2d4d02434b86761d1ce
ductt7575 <duc28072003@gmail.com>
2024-04-08 09:28:58 +0700

    React lifecycle

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,17 +3,28 @@ import './DisplayInfo.scss';
 import logo from './../logo.svg';
 
 class DisplayInfo extends React.Component {
-    state = {
-        isShowListUser: true,
-    };
+    constructor(props) {
+        console.log('constructor');
+        super(props);
+        this.state = {
+            isShowListUser: true,
+        };
+    }
 
     handleShowHide(event) {
         this.setState({
             isShowListUser: !this.state.isShowListUser,
         });
     }
-    handleDelete(event) {}
+    componentDidMount() {
+        console.log('Did mount');
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Did update');
+    }
     render() {
+        console.log('render');
+
         //Destructuring Array/Object
         const { listUsers } = this.props;
         return (
@@ -25,21 +36,19 @@ class DisplayInfo extends React.Component {
                     <>
                         {listUsers.map((user, index) => {
                             return (
-                                <>
-                                    <div key={user.id} className={+user.age >= 21 ? 'green' : 'red'}>
-                                        <div>My name is {user.name}</div>
-                                        <div>My age is {user.age}</div>
-                                        <div>I am studying in {user.major}</div>
-                                        <button
-                                            onClick={() => {
-                                                this.props.handleDeleteUser(user.id);
-                                            }}
-                                        >
-                                            Delete
-                                        </button>
-                                        <hr />
-                                    </div>
-                                </>
+                                <div key={user.id} className={+user.age >= 21 ? 'green' : 'red'}>
+                                    <div>My name is {user.name}</div>
+                                    <div>My age is {user.age}</div>
+                                    <div>I am studying in {user.major}</div>
+                                    <button
+                                        onClick={() => {
+                                            this.props.handleDeleteUser(user.id);
+                                        }}
+                                    >
+                                        Delete
+                                    </button>
+                                    <hr />
+                                </div>
                             );
                         })}
                     </>
