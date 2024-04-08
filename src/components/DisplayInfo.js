36e42109c2fe33812d9921f1f2496442faa756e8
ductt7575 <duc28072003@gmail.com>
2024-04-08 10:11:06 +0700

    UseState with react hook

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,61 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DisplayInfo.scss';
 import logo from './../logo.svg';
 
-class DisplayInfo extends React.Component {
-    constructor(props) {
-        console.log('constructor');
-        super(props);
-        this.state = {
-            isShowListUser: true,
-        };
-    }
+// class DisplayInfo extends React.Component {
+//     render() {
+//         console.log('render');
 
-    handleShowHide(event) {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser,
-        });
-    }
-    componentDidMount() {
-        console.log('Did mount');
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('Did update');
-    }
-    render() {
-        console.log('render');
+//         //Destructuring Array/Object
+//         const { listUsers } = this.props;
+//         return (
+//             <div className="display-info-container">
+//                 {true && (
+//                     <>
+//                         {listUsers.map((user, index) => {
+//                             return (
+//                                 <div key={user.id} className={+user.age >= 21 ? 'green' : 'red'}>
+//                                     <div>My name is {user.name}</div>
+//                                     <div>My age is {user.age}</div>
+//                                     <div>I am studying in {user.major}</div>
+//                                     <button
+//                                         onClick={() => {
+//                                             this.props.handleDeleteUser(user.id);
+//                                         }}
+//                                     >
+//                                         Delete
+//                                     </button>
+//                                     <hr />
+//                                 </div>
+//                             );
+//                         })}
+//                     </>
+//                 )}
+//             </div>
+//         );
+//     }
+// }
+// Khi viết function component thì ko viết this
+const DisplayInfo = (props) => {
+    const { listUsers } = props;
 
-        //Destructuring Array/Object
-        const { listUsers } = this.props;
-        return (
-            <div className="display-info-container">
-                <div onClick={(event) => this.handleShowHide(event)}>
-                    {this.state.isShowListUser === true ? 'Hide user list' : 'Show user list'}
-                </div>
-                {this.state.isShowListUser && (
-                    <>
-                        {listUsers.map((user, index) => {
-                            return (
-                                <div key={user.id} className={+user.age >= 21 ? 'green' : 'red'}>
-                                    <div>My name is {user.name}</div>
-                                    <div>My age is {user.age}</div>
-                                    <div>I am studying in {user.major}</div>
-                                    <button
-                                        onClick={() => {
-                                            this.props.handleDeleteUser(user.id);
-                                        }}
-                                    >
-                                        Delete
-                                    </button>
-                                    <hr />
-                                </div>
-                            );
-                        })}
-                    </>
-                )}
+    const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+    const handleShowHideListUser = () => {
+        // alert('click me');
+        setShowHideListUser(!isShowHideListUser);
+    };
+
+    return (
+        <div className="display-info-container">
+            <div>
+                <span onClick={() => handleShowHideListUser()}>
+                    {isShowHideListUser === true ? 'Hide list user' : 'Show list user'}
+                </span>
             </div>
-        );
-    }
-}
-
+            {isShowHideListUser && (
+                <>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <div key={user.id} className={+user.age >= 21 ? 'green' : 'red'}>
+                                <div>My name is {user.name}</div>
+                                <div>My age is {user.age}</div>
+                                <div>I am studying in {user.major}</div>
+                                <button
+                                    onClick={() => {
+                                        props.handleDeleteUser(user.id);
+                                    }}
+                                >
+                                    Delete
+                                </button>
+                                <hr />
+                            </div>
+                        );
+                    })}
+                </>
+            )}
+        </div>
+    );
+};
 export default DisplayInfo;
