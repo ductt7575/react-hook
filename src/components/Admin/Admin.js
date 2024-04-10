b96ed42f3cc75222731543c757dd71b981c21929
ductt7575 <duc28072003@gmail.com>
2024-04-10 09:33:14 +0700

    active when navigate

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 const Admin = (props) => {
-    return (
-        <div>
-            Admin components{' '}
-            <button>
-                <Link to="/" className="list">
-                    Go home page
-                </Link>
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      Admin components{' '}
+      <button>
+        <Link to="/" className="list">
+          Go home page
+        </Link>
+      </button>
+    </div>
+  );
 };
 export default Admin;
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 const User = (props) => {
-    return (
-        <div>
-            User components{' '}
-            <button>
-                <Link to="/" className="list">
-                    Go home page
-                </Link>
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      User components{' '}
+      <button>
+        <Link to="/" className="list">
+          Go home page
+        </Link>
+      </button>
+    </div>
+  );
 };
 export default User;
