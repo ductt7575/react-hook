ee920660e2a2f7d47a3e3b3b96a5def8a284a5dc
ductt7575 <duc28072003@gmail.com>
2024-04-11 13:47:43 +0700

    Use Toastify and Validate form

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
 import axios from 'axios';
-
+import { toast } from 'react-toastify';
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
 
@@ -33,9 +33,25 @@ const ModalCreateUser = (props) => {
     }
   };
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  };
+
   const handleSubmitCreateUser = async () => {
     // validate
-
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      toast.error('Invalid email');
+      return;
+    }
+    if (!password) {
+      toast.error('Invalid password');
+      return;
+    }
     //call apis
     const data = new FormData();
     data.append('email', email);
@@ -45,7 +61,15 @@ const ModalCreateUser = (props) => {
     data.append('userImage', image);
 
     let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-    console.log(res);
+    console.log(res.data);
+    if (res.data && res.data.EC === 0) {
+      toast.success(res.data.EM);
+      handleClose();
+    }
+
+    if (res.data && res.data.EC !== 0) {
+      toast.error(res.data.EM);
+    }
   };
 
   return (
