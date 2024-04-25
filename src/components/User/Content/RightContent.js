171e2bf449f6e4893ad36b4aafbd86998572384e
ductt7575 <duc28072003@gmail.com>
2024-04-25 11:16:25 +0700

    countdown and convert second to hhmmss

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,15 +1,21 @@
+import CountDown from './CountDown';
+
 const RightContent = (props) => {
   const { dataQuiz } = props;
-  console.log(dataQuiz);
+  const onTimeUp = () => {
+    props.handleFinish();
+  };
   return (
     <>
-      <div className="main-timer">10:10</div>
+      <div className="main-timer">
+        <CountDown onTimeUp={onTimeUp} />
+      </div>
       <div className="main-question row gx-1">
         {dataQuiz &&
           dataQuiz.length > 0 &&
           dataQuiz.map((item, index) => {
             return (
-              <div className="col-lg-2 col-md-4 col-6">
+              <div key={`question-abc-${index}`} className="col-lg-2 col-md-4 col-6">
                 <p className="question">{index + 1}</p>
               </div>
             );
