b9208681b8c438362a41c037afc37f823df1c7dd
ductt7575 <duc28072003@gmail.com>
2024-05-02 11:54:45 +0700

    fix bug order answer & UI dashboard with chart

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -1,4 +1,69 @@
+import './DashBoard.scss';
+import { BarChart, Bar, CartesianGrid, Legend, Tooltip, YAxis, XAxis, ResponsiveContainer } from 'recharts';
 const Dashboard = (props) => {
-  return <div>Dash board</div>;
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+    },
+  ];
+
+  return (
+    <div className="dashboard-container">
+      <div className="title">Analytics Dashboard</div>
+      <div className="content">
+        <div className="c-left row">
+          <div className="col-md-6 col-sm-12 child">Total Users</div>
+          <div className="col-md-6 col-sm-12 child">Total Quizzes</div>
+          <div className="col-md-6 col-sm-12 child">Total Questions</div>
+          <div className="col-md-6 col-sm-12 child">Total Answers</div>
+        </div>
+        <div className="c-right">
+          <ResponsiveContainer width="95%" height="100%">
+            <BarChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" fill="#8884d8" />
+              <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Dashboard;
