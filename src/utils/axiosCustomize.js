ff6cd9f2aef66153de7d6fca83f6714b6c4e0e54
ductt7575 <duc28072003@gmail.com>
2024-05-03 19:39:18 +0700

    Fix bug accesstoken expired

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -38,6 +38,11 @@ instance.interceptors.response.use(
   },
   function (error) {
     NProgress.done();
+    //token expired
+    if (error.response.data && error.response.data.EC === -999) {
+      window.location.href = '/login';
+    }
+
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response && error.response.data ? error.response.data : Promise.reject(error);
